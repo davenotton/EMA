153b38673dd8c5c85bb17ca017618f74bb653bd9
David Notton <39244641+davenotton@users.noreply.github.com>
2018-05-18 17:44:06 +0100

    Q3B query working
    no server side validation
    no styleing

--- Q3B/query.php
+++ Q3B/query.php
@@ -5,7 +5,7 @@
       <title>Search results</title>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="styles.css">
 
    </head>
    <body>
@@ -13,50 +13,49 @@
 <?php
 
 $databasename ='walkingclub.sqlite';
-$db = new SQLite3($databasename);
+$db = new SQLite3($databasename);            ///connect to database.
 
 if($db === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
   }
 
-if(isset($_POST['search-term']) && $_POST['search-term'] != ""){
+if(isset($_POST['search-term']) && $_POST['search-term'] != ""){ /// check search input is nopt empty.
 
-$webdata['query'] = $_POST['search-term'];
-
-//$str = "17";
-$num =(int)7;
-echo $num;
-
+$webdata = $_POST['search-term']; /// assingn variable.
 echo "<p>Searching...</p>";
-$sql = "SELECT name, walk_date FROM walk WHERE walk_date > (SELECT DATETIME('now','$num days'))";
-//$sql = "SELECT name, walk_date FROM walk WHERE walk_date > (SELECT STRFTIME) ";
-//$sql = "SELECT name, walk_date FROM walk WHERE walk_date > (SELECT DATETIME('now', '-7day'))";
-//$sql = "SELECT name, walk_date FROM walk WHERE walk_date BETWEEN :query AND 2018-05-17";
-//$sql = "SELECT name, walk_date FROM walk WHERE walk_date LIKE :query";
 
-$stmt = $db->prepare($sql);
-$stmt->bindValue(":query" , '%'.$webdata['query'].'%',SQLITE3_TEXT);
-$result = $stmt->execute();
+/// prepare statement.
 
-$currentDate = date("Y-m-d"); /// date test
-echo "<p>$currentDate</p>";
-///$currentDate .= +1 d;
-echo "<p>$currentDate</p>";
-
-while ($row = $result->fetchArray()){
-  //for($currentDate)
-  echo '<p>' . "Name: " . htmlspecialchars($row['name']) . " | Walk-date: " . htmlspecialchars($row['walk_date']) . '</p>';
-  //echo"<input type='checkbox' name='select-result'>"."Select to edit.";
+if ($webdata < 0){ ///number is negative.
+$sql = "SELECT name, walk_date,start_time, leader, meeting_point, meeting_latlong, distance, route, notes, status
+FROM walk WHERE walk_date BETWEEN DATETIME('now','$webdata days','-1 days') AND DATETIME('now')";
+}
+else { ///number is negative.
+  $sql = "SELECT name, walk_date, start_time, leader, meeting_point, meeting_latlong, distance, route, notes, status
+   FROM walk WHERE walk_date BETWEEN DATETIME('now') AND DATETIME('now', '$webdata days')";
 }
 
-if(!$row = $result->fetchArray()) {
+$stmt = $db->prepare($sql);
+/// bind statement.
+$stmt->bindValue(":query" , '%'.$webdata.'%',SQLITE3_TEXT);
+/// execute statement.
+$result = $stmt->execute();
+
+/// display query data to user.
+while ($row = $result->fetchArray()){
+  echo '<p>' . "Name: " . htmlspecialchars($row['name']) . " | Walk-date: " . htmlspecialchars($row['walk_date']) . " | Start-time: " . htmlspecialchars($row['start_time']) .
+   " | Leader: " . htmlspecialchars($row['leader']) . " | Meeting-point: " . htmlspecialchars($row['meeting_point']) . " | Meeting-point Lat & Long: " . htmlspecialchars($row['meeting_latlong']) .
+   " | Distance: " . htmlspecialchars($row['distance']) . " | Route: " . htmlspecialchars($row['route']) . " | Notes: " . htmlspecialchars($row['notes']) . " | Status: " . htmlspecialchars($row['status']) .'</p>';
+}
+
+if(!$row = $result->fetchArray()) { /// query data dose not exist.
   echo "<p>No data could be found - Please enter new search.</p>";
 }
 }
-else{
+else{ ///incorrect search term entered.
   echo '<p>Please enter a search query.</p>';
 }
-
 ?>
+
 </body>
 </html>
