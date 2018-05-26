6eae53ace4eb31a8632b8899d3d101ab68be6412
David Notton <39244641+davenotton@users.noreply.github.com>
2018-05-26 18:44:53 +0100

    Validation working

--- Q3B/validateData.php
+++ Q3B/validateData.php
@@ -5,35 +5,72 @@ $formerror = '';
 $valid = TRUE ; // set a variable to true at the start. If we find and error change it to false. At the end if there are any error messages, return the form and data and messages, but don't save.
 //$firstname = $webdata['firstname'];
 if (isset($_POST['name'])) {
-if (!preg_match("/^[a-zA-Z ]{1,30}$/",$_POST['name'])) {
-  $formerror = '<span class="warn" >Not valid on server: Only letters and white space allowed </span>';
+if (!preg_match("/[^[(){};<>]+/",$_POST['name'])) {
+  $formerror = '<span class="warn" >Name: Not valid on server: Symbols: () {} <> Not allowed. - Only letters and digits allowed.  </span>';
   //echo "Only letters and white space allowed";
   $valid = FALSE ;
 }}
-//if (isset($webdata['lastname'] )) {
-//if (!preg_match("/^[a-zA-Z ]{1,30}$/",$webdata['lastname'])) {
-//  $formerror['lastname'] = '<span class="warn" >Not valid on server: Only letters and white space allowed</span>';
-//  $valid = FALSE;
-//}}
+if (isset($_POST['walk_date'])) {
+if (!preg_match("/[^[(){};<>]+/",$_POST['walk_date'])) {
+  $formerror = '<span class="warn" >Walk-date: Not valid on server: Symbols: () {} <> Not allowed. - Only letters and digits allowed. </span>';
+  //echo "Only letters and white space allowed";
+  $valid = FALSE ;
+}}
+if (isset($_POST['start_time'])) {
+if (!preg_match("/[^[(){};<>]+/",$_POST['start_time'])) {
+  $formerror = '<span class="warn" >Start-time: Not valid on server: Symbols: () {} <> Not allowed. - Only letters and digits allowed. </span>';
+  //echo "Only letters and white space allowed";
+  $valid = FALSE ;
+}}
+if (isset($_POST['leader'])) {
+if (!preg_match("/[^[(){};<>]+/",$_POST['leader'])) {
+  $formerror = '<span class="warn" >Leader: Not valid on server: Symbols: () {} <> Not allowed. - Only letters and digits allowed. </span>';
+  //echo "Only letters and white space allowed";
+  $valid = FALSE ;
+}}
+if (isset($_POST['meeting_point'])) {
+if (!preg_match("/[^[(){};<>]+/",$_POST['meeting_point'])) {
+  $formerror = '<span class="warn" >Meeting-point: Not valid on server: Symbols: ( ) { } < > Not allowed. - Only letters and digits allowed. </span>';
+  //echo "Only letters and white space allowed";
+  $valid = FALSE ;
+}}
+if (isset($_POST['meeting_point_lat'])) {
+if (!preg_match("/[^[(){};<>]+/",$_POST['meeting_point_lat'])) {
+  $formerror = '<span class="warn" >Latitude: Not valid on server: Symbols: ( ) { } < > Not allowed. - Only letters and digits allowed. </span>';
+  //echo "Only letters and white space allowed";
+  $valid = FALSE ;
+}}
+if (isset($_POST['meeting_point_long'])) {
+if (!preg_match("/[^[(){};<>]+/",$_POST['meeting_point_long'])) {
+  $formerror = '<span class="warn" >Longitude: Not valid on server: Symbols: ( ) { } < > Not allowed. - Only letters and digits allowed. </span>';
+  //echo "Only letters and white space allowed";
+  $valid = FALSE ;
+}}
+if (isset($_POST['distance'])) {
+if (!preg_match("/[^[(){};<>]+/",$_POST['distance'])) {
+  $formerror = '<span class="warn" >Distance: Not valid on server: Symbols: ( ) { } < > Not allowed. - Only letters and digits allowed. </span>';
+  //echo "Only letters and white space allowed";
+  $valid = FALSE ;
+}}
+if (isset($_POST['route'])) {
+if (!preg_match("/[^[(){};<>]+/",$_POST['route'])) {
+  $formerror = '<span class="warn" >Route: Not valid on server: Symbols: ( ) { } < > Not allowed. - Only letters and digits allowed. </span>';
+  //echo "Only letters and white space allowed";
+  $valid = FALSE ;
+}}
+if (isset($_POST['notes'])) {
+if (!preg_match("/[^[(){};<>]+/",$_POST['notes'])) {
+  $formerror = '<span class="warn" >Notes: Not valid on server: Symbols: ( ) { } < > Not allowed. - Only letters and digits allowed. </span>';
+  //echo "Only letters and white space allowed";
+  $valid = FALSE ;
+}}
 
-//if (isset($webdata['email'] )) {
-//if (!filter_var($webdata['email'], FILTER_VALIDATE_EMAIL)) {
-//	$formerror['email'] = '<span class="warn" >Not valid on server: invalid email format</span>';
-  //echo "Invalid email format";
-//  $valid = FALSE;
-//}}
-//if (isset($webdata['telephone'] )) {
-//if (!preg_match("/^[0-9]{8,10}$/",$webdata['telephone'])) {
-  //$formerror['telephone'] = '<span class="warn" >Not valid on server: Only numbers allowed</span>';
-  //$valid = FALSE;
-//}}
-
-//function validateDate($date, $format = 'Y-m-d H:i:s')
-//{
-//    $d = DateTime::createFromFormat($format, $date);
-//    return $d && $d->format($format) == $date;
-//    $valid = FALSE;
-//}//http://php.net/manual/en/function.checkdate.php
+function validateDate($date, $format = 'Y-m-d H:i:s')
+{
+   $d = DateTime::createFromFormat($format, $date);
+   return $d && $d->format($format) == $date;
+   $valid = FALSE;
+}//http://php.net/manual/en/function.checkdate.php
 //validateDate('2012-02-28', 'Y-m-d')
 //validateDate('2012-02-28 12:12:12')
 //validateDate('2012-02-28T12:12:12+02:00', 'Y-m-d\TH:i:sP')
